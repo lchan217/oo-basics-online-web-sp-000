c0c967807a4ab237bf402bb8f5167c3384f6ad26
Lizzie Chan <lchan217@gmail.com>
2019-02-02 02:17:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,19 +2,19 @@ class Book
   def initialize(title)
     @title = title
   end
-<<<<<<< HEAD
+
   attr_reader :title
   attr_accessor :author, :page_count, :genre
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end 
-=======
+
   def title 
     @title = title
   end
   attr_accessor :author_name
   attr_accessor :page_count
   attr_accessor :genre
->>>>>>> 2a388c563f6ba99918c7156a06877b6dc0aa8669
+
 end
