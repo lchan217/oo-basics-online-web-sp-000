c9e791e7ef4ea26e49a8056075d00ed671e8b837
Lizzie Chan <lchan217@gmail.com>
2019-02-02 02:18:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,10 @@
 class Shoe
-  attr_accessor :brand 
+  attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
   end 
 end 
